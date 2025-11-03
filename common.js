e6c1f2594c0fdd51a64e5f27a3f75e7794e01ee8
takooooooooo <130232175+takooooooooo@users.noreply.github.com>
2025-11-03 17:30:08 +0900

    Update common.js

--- common.js
+++ common.js
@@ -3,12 +3,12 @@
  */
 
 // QRコードに関する設定
-const QR_PAYLOAD_SIZE = 700; // 1つのQRコードに含まれるペイロードの最大バイト数
+const QR_PAYLOAD_SIZE = 400; // 1つのQRコードに含まれるペイロードの最大バイト数
 const QR_TRANSMISSION_INTERVAL = 500; // QRコードを切り替える基本時間間隔(ミリ秒)
 
 const QR_CONFIG = {
     errorCorrectionLevel: 'M', // 誤り訂正レベル
-    typeNumber: 30, // QRコードのバージョン (40が最大)
+    typeNumber: 0, // QRコードのバージョン (0で自動選択)
 };
 
 // WebSocketに関する設定
